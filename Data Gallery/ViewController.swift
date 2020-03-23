a1cd6222c014f702d72a5c82b89867a38b632221
Fazle Rabbi Linkon <fazle.linkon@dreamonline.co.jp>
2020-03-23 12:36:48 +0600

    Initial Commit

---
+++ Data Gallery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Data Gallery
+//
+//  Created by DreamOnline on 23/3/20.
+//  Copyright © 2020 Fazle Rabbi Linkon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Data GalleryTests/Data_GalleryTests.swift
@@ -0,0 +1,34 @@
+//
+//  Data_GalleryTests.swift
+//  Data GalleryTests
+//
+//  Created by DreamOnline on 23/3/20.
+//  Copyright © 2020 Fazle Rabbi Linkon. All rights reserved.
+//
+
+import XCTest
+@testable import Data_Gallery
+
+class Data_GalleryTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
